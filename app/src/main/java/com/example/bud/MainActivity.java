331f4fc39ded2440c982f58bb0f02a69faad4456
Asdmaster31 <45410628+Asdmaster31@users.noreply.github.com>
2020-05-04 16:51:07 -0500

    Added a private DrawerLayout to the MainActivity. Added a few other things in there too for the sake of interface.

--- app/src/main/java/com/example/bud/MainActivity.java
+++ app/src/main/java/com/example/bud/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.bud;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout
 
     DatabaseHelper databaseHelper;
 
@@ -13,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //databaseHelper = new DatabaseHelper(this);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
