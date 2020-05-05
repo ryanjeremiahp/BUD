b5d46f8740b480d68033665345d673982294cf9d
Asdmaster31 <45410628+Asdmaster31@users.noreply.github.com>
2020-05-04 20:36:43 -0500

    Running version, where the homescreen displays the first thing on the side panel. Will update the homepage with Kali's stuff though probz. Just glad it runs again.

--- app/src/main/java/com/example/bud/HelpFragment.java
+++ app/src/main/java/com/example/bud/HelpFragment.java
@@ -8,10 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FinanceFragment extends Fragment {
+public class HelpFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_finance, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
--- app/src/main/java/com/example/bud/MainActivity.java
+++ app/src/main/java/com/example/bud/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.bud;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     DatabaseHelper database;
@@ -22,12 +25,39 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_closer);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FinanceFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_finances);
+        }
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.nav_finances:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FinanceFragment()).commit();
+                break;
+            case R.id.nav_help:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new HelpFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
